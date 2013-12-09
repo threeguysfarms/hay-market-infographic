494cc27417fc232dc33c2adacd58f9587d933ac6
Alex Notov <anotov@gmail.com>
2013-12-08 19:24:05 -0800

    Now pulling the first 100 reports for all regions!
    Phew great success.
    
    Removed all unprocessed reports from repo.

--- pull_reports.rb
+++ pull_reports.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'open-uri'
+require 'net/http'
 
 Bundler.require
 
-report_names = ["MG_GR310", 
+# get the report id components into a reports hash
+reports = ["MG_GR310", 
   "GX_GR312", 
   "ML_GR311", 
   "GL_GR310", 
@@ -33,12 +35,46 @@ report_names = ["MG_GR310",
   "RH_GR310", 
   "ML_GR310", 
   "TO_GR325", 
-  "TO_GR310"]
+  "TO_GR310"].map do |name|
+    { 
+      location_code: name[0..1], 
+      commodity_code: "GR",
+      wire_code: name[-3..-1]
+    }  
+  end
 
-report_names.each do |name|
-  open("unprocessed_reports/#{name}20131205.txt", "wb") do |file|
-    open("http://search.ams.usda.gov/mndms/2013/12/#{name}20131205.TXT") do |uri|
-      file.write(uri.read)
+reports.each do |report|
+  a = Mechanize.new do |agent|
+    agent.user_agent_alias = "Mac Safari"
+    agent.follow_redirect = :all
+  end
+
+  a.get("http://search.ams.usda.gov/mnsearch/mnsearch.aspx") do |page|
+    search_results = a.post "http://search.ams.usda.gov/mnsearch/mnsearch.aspx", {
+      "locationCode" => report[:location_code],
+      "commodityCode" => report[:commodity_code],
+      "wireCode" => report[:wire_code], 
+      "documentType" => "MNDMS_TEXT", 
+      "timeRange" => "Any", 
+      "resultCount" => "100", 
+      "sortField" => "Date", 
+      "sortOrder" => "true", 
+      "goButton.x" => "8", 
+      "goButton.y" => "7", 
+      "__EVENTVALIDATION" => page.parser.at("input#__EVENTVALIDATION")["value"], 
+      "__VIEWSTATE" => page.parser.at("input#__VIEWSTATE")["value"]
+    }
+
+    search_results.parser.css("table#MNResultGrid tr td:first a").each do |report_link|
+      href = report_link["href"].downcase
+      filename = "unprocessed_reports/#{File.basename(href)}"
+      open(filename, "wb") do |file|
+        open(href) do |uri|
+          puts "About to write out file #{filename}"
+          file.write(uri.read)
+          puts "Done writing out file #{filename}"
+        end
+      end
     end
   end
 end
